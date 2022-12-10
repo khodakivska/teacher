b6d4bfaca6386b0c1c67f5e24be2c6b061c6fe7f
khodakivska <120274169+khodakivska@users.noreply.github.com>
2022-12-10 20:30:46 +0200

    Add files via upload

---
+++ scripts/pres.js
@@ -0,0 +1,190 @@
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-01").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-01').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-01").data("pres") +
+                 '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-02").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-02').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-02").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-03").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-03').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-03").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-04").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-04').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-04").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-05").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-05').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-05").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-06").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-06').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-06").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-07").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-07').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-07").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-08").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-08').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-08").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-09").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-09').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-09").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-10").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-10').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-10").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-11").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-11').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-11").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-12").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-12').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-12").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-13").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-13').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-13").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-14").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-14').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-14").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-15").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-15').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-15").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-16").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-16').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-16").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-17").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-17').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-17").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-18").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-18').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-18").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
+jQuery(document).ready(function ($) {
+    (function initPlayPres() {
+        $(".pres-cover-19").on("click", function () {
+            $(this).fadeOut().siblings('.presentation-19').html(
+                '<iframe src="https://docs.google.com/presentation/d/e/' + $(".pres-cover-19").data("pres") +
+                '/embed?start=false&loop=false&delayms=5000" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" frameborder="0"></iframe>'
+            );
+        });
+    })();
+});
